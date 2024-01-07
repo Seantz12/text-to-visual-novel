db0f77b5f1f5601d8ba06c912f22de4040613893
Tim Zhou <5866950+ttzhou@users.noreply.github.com>
2024-01-07 17:50:01 -0500

    Adjust parser for more test cases (#2)
    * Parser is now able to handle "contiguous" characters after the stop character, e.g. annotations of form '.[a][b]'

--- ttvn/parse.py
+++ ttvn/parse.py
@@ -1,13 +1,19 @@
 class Parser:
 
     STOP_CHARS = set(".?!")
-    POST_STOP_CHARS = set(")\"")
+    POST_STOP_CHARS = set(")\"[")
     LINEBREAK_CHARS = set("\n\r")
     SPACE_CHARS = set(" ")
     WHITESPACE_CHARS = LINEBREAK_CHARS | SPACE_CHARS
 
     UPPERCASE_CHARS = set("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
     LOWERCASE_CHARS = set("abcdefghijklmnopqrstuvwxyz")
+
+    ALPHA_CHARS = UPPERCASE_CHARS | LOWERCASE_CHARS
+    DIGIT_CHARS = set("0123456789")
+
+    ALPHANUMERIC_CHARS = ALPHA_CHARS | DIGIT_CHARS
+    SENTENCE_START_CHARS = UPPERCASE_CHARS | DIGIT_CHARS
 
     LINES_PER_PARAGRAPH = 6
 
@@ -39,17 +45,24 @@ class Parser:
         sentence = ""
         n = len(text)
         prev_c = None
-        for i, c in enumerate(text):
-            self.__raw += c
+        i = 0
+        while i < n:
+            c = text[i]
             prev_c = text[i - 1] if i > 0 else ''
             next_c = text[i + 1] if i < n - 1 else ''
+            self.__raw += c
 
             # We only consider a sentence possibly done if we hit
             # whitespace or 'post-stop' characters.
             if c in (self.WHITESPACE_CHARS | self.POST_STOP_CHARS):
                 if prev_c in self.WHITESPACE_CHARS:
                     if c in self.POST_STOP_CHARS:
-                        sentence += c
+                        while i < n:
+                            c = text[i]
+                            sentence += c
+                            if c in self.WHITESPACE_CHARS:
+                                break
+                            i += 1
                     elif (
                         c in self.LINEBREAK_CHARS 
                         and prev_c in self.LINEBREAK_CHARS
@@ -61,18 +74,24 @@ class Parser:
                         if paragraph:
                             self.__parsed.append(paragraph)
                             paragraph = []
-                    continue
                 elif prev_c in self.STOP_CHARS:
                     # If we are at a stop char, we should look ahead
                     # to confirm a new sentence is ahead. We do this 
-                    # naively by checking if first alpha character up
-                    # ahead is uppercase.
+                    # naively by checking if first alphanumeric character up
+                    # ahead is a sentence 'start' character.
+                    if c in self.POST_STOP_CHARS:
+                        while i < n:
+                            c = text[i]
+                            if c in self.WHITESPACE_CHARS:
+                                break
+                            sentence += c
+                            i += 1
                     new_sentence_ahead = False
                     look_ahead = i
                     while look_ahead < n:
                         next_c = text[look_ahead]
-                        if next_c in self.UPPERCASE_CHARS | self.LOWERCASE_CHARS:
-                            if next_c in self.UPPERCASE_CHARS:
+                        if next_c in self.ALPHANUMERIC_CHARS:
+                            if next_c in self.SENTENCE_START_CHARS:
                                 new_sentence_ahead = True
                             break
                         look_ahead += 1
@@ -82,7 +101,7 @@ class Parser:
                             paragraph.append(sentence.strip())
                             sentence = ""
                     else:
-                        sentence += c
+                        sentence += c if c not in self.LINEBREAK_CHARS else ''
                     if len(paragraph) == self.LINES_PER_PARAGRAPH:
                         self.__parsed.append(paragraph)
                         paragraph = []
@@ -91,6 +110,7 @@ class Parser:
                     sentence += '' if c in self.LINEBREAK_CHARS else c
             else:
                 sentence += c
+            i += 1
         # Cleanup any buffers.
         if sentence:
             paragraph.append(sentence)
